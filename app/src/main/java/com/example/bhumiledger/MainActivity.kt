c509fb61e747d0eabe61cde16657ac10e5e9a35a
vinay-singh-dev <vv4011273@gmail.com>
2026-02-10 18:35:55 +0530

    added adb and fixed shell path

--- app/src/main/java/com/example/bhumiledger/MainActivity.kt
+++ app/src/main/java/com/example/bhumiledger/MainActivity.kt
@@ -11,11 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bhumiledger.data.BhumiLedgerContainer
 import com.example.bhumiledger.theme.BhumiLedgerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val container = BhumiLedgerContainer()
+        val viewModel = MainViewModel(container)
+        viewModel.testOwnershipFlow()
+
         enableEdgeToEdge()
         setContent {
             BhumiLedgerTheme {
@@ -24,6 +30,8 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+
                 }
             }
         }
--- app/src/main/java/com/example/bhumiledger/MainViewmodel.kt
+++ app/src/main/java/com/example/bhumiledger/MainViewmodel.kt
@@ -1,5 +1,6 @@
 package com.example.bhumiledger
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.bhumiledger.data.BhumiLedgerContainer
 import com.example.bhumiledger.domain.model.OwnershipClaim
@@ -24,5 +25,38 @@ class MainViewModel(
 
     fun getOwnershipHistory(parcelId: String): DomainResult<List<RegistryEntry>> {
         return container.getOwnershipHistory(parcelId)
+    }
+
+    fun testOwnershipFlow() {
+        val parcelId = "parcel-26112006"
+        val claimantId = "Aneesh"
+
+        val submitResult = container.submitOwnershipClaim(parcelId, claimantId)
+
+        if (submitResult is DomainResult.Success) {
+
+            val claim = submitResult.data
+
+            val verifyResult = container.verifyOwnershipClaim(claim.id)
+
+            if (verifyResult is DomainResult.Success) {
+                val verifiedClaim = verifyResult.data
+
+                val registryResult = container.createRegistryEntry(verifiedClaim)
+                if (registryResult is DomainResult.Success) {
+
+                    val historyResult = container.getOwnershipHistory(parcelId)
+                    if (historyResult is DomainResult.Success) {
+                        val history = historyResult.data
+
+                        Log.d("BhumiLedger", "Ownership history size: ${history.size}")
+                        Log.d("BhumiLedger", "Current Owner: ${history.last().ownerId}")
+
+                    }
+                }
+
+            }
+        }
+
     }
 }
