9ee9f8778e34015a8ae87e1c6be49e96dbe6b1fc
vinay-singh-dev <vv4011273@gmail.com>
2026-02-10 11:18:12 +0530

    data: implement in-memory claim repository

---
+++ data/src/main/java/com/example/bhumiledger/data/repository/InMemoryClaimRepository.kt
@@ -0,0 +1,32 @@
+package com.example.bhumiledger.data.repository
+
+import com.example.bhumiledger.domain.model.ClaimStatus
+import com.example.bhumiledger.domain.model.OwnershipClaim
+import com.example.bhumiledger.domain.repository.ClaimRepository
+
+class InMemoryClaimRepository: ClaimRepository {
+
+    private val claims = mutableListOf<OwnershipClaim>()
+    override fun getPendingClaimForParcel(parcelId: String): OwnershipClaim? {
+        return claims.find{ it.parcelId == parcelId && it.status == ClaimStatus.PENDING}
+
+    }
+
+    override fun saveClaim(claim: OwnershipClaim) {
+        claims.add(claim)
+    }
+
+    override fun getClaimById(claimId: String): OwnershipClaim? {
+       for (claim in claims)
+           if(claim.id == claimId)
+               return claim
+        return null
+    }
+
+    override fun updateClaim(claim: OwnershipClaim) {
+        val index = claims.indexOfFirst { it.id == claim.id}
+        if(index != -1)
+            claims[index] = claim
+
+    }
+}
